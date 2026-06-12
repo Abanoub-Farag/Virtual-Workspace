9fc6776993de12e08693bb116e0c0617aed496f2
Abanoub-Farag <abanoubfarag02@gmail.com>
2026-06-12 12:15:55 +0300

    feat(profile): added validation on profile attributes, change the post request to put request for updating profile

--- backend/src/main/java/app/virtual_workspace/accounts/controllers/ProfileController.java
+++ backend/src/main/java/app/virtual_workspace/accounts/controllers/ProfileController.java
@@ -4,13 +4,14 @@ import app.virtual_workspace.accounts.dtos.profile.UpdateUserProfileDto;
 import app.virtual_workspace.accounts.dtos.profile.UserProfileDto;
 import app.virtual_workspace.accounts.services.ProfileService;
 import app.virtual_workspace.accounts.services.UserAuthService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,7 +22,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class ProfileController {
 
     private final ProfileService profileService;
-    private final UserAuthService userAuthService;
 
     @GetMapping("/{userId}")
     public ResponseEntity<UserProfileDto> getProfile(@PathVariable Long userId){
@@ -30,10 +30,11 @@ public class ProfileController {
         return ResponseEntity.status(HttpStatus.OK).body(profile);
     }
 
-    @PostMapping("/{userId}")
+    @PutMapping("/{userId}")
     @PreAuthorize("@userAuthService.isOwner(#userId)")
     public ResponseEntity<UserProfileDto> updateProfile(
-            @PathVariable Long userId, @RequestBody UpdateUserProfileDto updateDto
+            @PathVariable Long userId,
+            @Valid @RequestBody UpdateUserProfileDto updateDto
     ){
         UserProfileDto profile = profileService.updateProfile(userId, updateDto);
 
--- backend/src/main/java/app/virtual_workspace/accounts/dtos/profile/UpdateUserProfileDto.java
+++ backend/src/main/java/app/virtual_workspace/accounts/dtos/profile/UpdateUserProfileDto.java
@@ -1,6 +1,8 @@
 package app.virtual_workspace.accounts.dtos.profile;
 
 import app.virtual_workspace.accounts.models.enums.Gender;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -13,10 +15,21 @@ import java.time.LocalDate;
 @NoArgsConstructor
 @Getter
 @Setter
+@Builder
 public class UpdateUserProfileDto {
+
+    @Size(max = 300, message = "Bio must not exceed 300 characters")
     private String bio;
+
     private Gender gender;
+
     private LocalDate dateOfBirth;
+
+    @NotBlank(message = "First name is required")
+    @Size(min = 3, max = 20, message = "First name must be between 3 and 20 characters")
     private String firstName;
+
+    @NotBlank(message = "Last name is required")
+    @Size(min = 3, max = 20, message = "Last name must be between 3 and 20 characters")
     private String lastName;
 }
