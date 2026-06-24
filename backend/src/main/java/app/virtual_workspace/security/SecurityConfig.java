c30f53a1725d0c3b35c0735f73419b3fc9395114
Abanoub-Farag <AbanoubFarag02@gmail.com>
2026-06-24 10:48:27 +0300

    chore: add openapi docs for api documentation - feat: add api documentation

--- backend/src/main/java/app/virtual_workspace/security/SecurityConfig.java
+++ backend/src/main/java/app/virtual_workspace/security/SecurityConfig.java
@@ -28,6 +28,16 @@ public class SecurityConfig {
             "/api/v1/auth/login",
             "/api/v1/auth/register",
             "/api/v1/auth/refresh",
+            "/v2/api-docs",
+            "/v3/api-docs",
+            "/v3/api-docs/**",
+            "/swagger-resources",
+            "/swagger-resources/**",
+            "/configuration/ui",
+            "/configuration/security",
+            "/swagger-ui/**",
+            "/webjars/**",
+            "/swagger-ui.html",
             "/error"
     };
 
