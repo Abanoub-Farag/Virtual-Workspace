3d8aa677b08ab1cfef49de23bb877b24d5b776e5
Abanoub-Farag <AbanoubFarag02@gmail.com>
2026-06-30 11:40:09 +0300

    fix(security): add EnableMethodSecurity for PreAuthorize annotations

--- backend/src/main/java/app/virtual_workspace/Application.java
+++ backend/src/main/java/app/virtual_workspace/Application.java
@@ -3,9 +3,11 @@ package app.virtual_workspace;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
 @SpringBootApplication
 @EnableCaching
+@EnableMethodSecurity
 public class Application {
 
 	public static void main(String[] args) {
