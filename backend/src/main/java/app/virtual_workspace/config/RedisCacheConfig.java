fb4c2ecf6e739169eb53733709219e4f0099a462
Abanoub Farag <AbanoubFarag02@gmail.com>
2026-08-07 11:39:04 +0300

    fix: update deprecated redis serializer

--- backend/src/main/java/app/virtual_workspace/config/RedisCacheConfig.java
+++ backend/src/main/java/app/virtual_workspace/config/RedisCacheConfig.java
@@ -6,8 +6,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.GenericJacksonJsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
+import tools.jackson.databind.ObjectMapper;
 
 import java.time.Duration;
 
@@ -20,7 +21,7 @@ public class RedisCacheConfig {
                 .defaultCacheConfig()
                 .entryTtl(Duration.ofMinutes(10))
                 .disableCachingNullValues()
-                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()));
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJacksonJsonRedisSerializer(new ObjectMapper())));
 
         return RedisCacheManager.builder(connectionFactory)
                 .cacheDefaults(configuration)
