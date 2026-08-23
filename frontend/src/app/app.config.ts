d7f0e103ee137bbde866a00169892aa5739ee74e
Abanoub Farag <AbanoubFarag02@gmail.com>
2026-08-23 13:36:36 +0300

    feat: Add configs for vercel

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -2,7 +2,8 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
-import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
+import { ngrokInterceptor } from './core/interceptors/ngrok.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -10,5 +11,6 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideClientHydration(),
     provideHttpClient(withFetch()),
+    provideHttpClient(withInterceptors([ngrokInterceptor]))
   ],
 };
