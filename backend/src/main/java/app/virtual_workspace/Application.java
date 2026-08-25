d5bde71d03fa657f0bbe161823c0546904d8fc74
Abanoub Farag <AbanoubFarag02@gmail.com>
2026-08-25 15:58:31 +0300

    refactor: Enable scheduling

--- backend/src/main/java/app/virtual_workspace/Application.java
+++ backend/src/main/java/app/virtual_workspace/Application.java
@@ -9,7 +9,7 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 @SpringBootApplication
 @EnableCaching
 @EnableMethodSecurity
-//@EnableScheduling
+@EnableScheduling
 public class Application {
 
 	public static void main(String[] args) {
