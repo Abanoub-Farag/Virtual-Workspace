2d716edd787abb90957c0f65f2088274a10ad1cb
Abanoub Farag <AbanoubFarag02@gmail.com>
2026-08-26 15:58:24 +0300

    fix(auth): handle refresh token updates and add missing expiration check

--- backend/src/main/java/app/virtual_workspace/accounts/services/RefreshTokenService.java
+++ backend/src/main/java/app/virtual_workspace/accounts/services/RefreshTokenService.java
@@ -1,11 +1,14 @@
 package app.virtual_workspace.accounts.services;
 
 import app.virtual_workspace.accounts.models.RefreshToken;
+import app.virtual_workspace.accounts.models.User;
 import app.virtual_workspace.accounts.repositories.RefreshTokenRepository;
 import app.virtual_workspace.accounts.repositories.UserRepository;
+import app.virtual_workspace.exceptions.custom.ResourceNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.Instant;
 import java.util.UUID;
@@ -19,11 +22,19 @@ public class RefreshTokenService {
     private final RefreshTokenRepository refreshTokenRepository;
     private final UserRepository userRepository;
 
-    public RefreshToken createRefreshToken(Long userId){
-        var refreshToken = new RefreshToken();
-        refreshToken.setUser(userRepository.findUserById(userId).get());
-        refreshToken.setExpiryDate(Instant.now().plusMillis(refreshExpiration));
+    @Transactional
+    public RefreshToken createRefreshToken(Long userId) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new ResourceNotFoundException("User not found"));
+
+        RefreshToken refreshToken = refreshTokenRepository.findByUserId(userId)
+                .orElseGet(() -> RefreshToken.builder()
+                        .user(user)
+                        .build());
+
         refreshToken.setToken(UUID.randomUUID().toString());
+        refreshToken.setExpiryDate(Instant.now().plusMillis(refreshExpiration));
+
         return refreshTokenRepository.save(refreshToken);
     }
 
